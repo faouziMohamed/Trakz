b5d0bddadc8cef7dfcc27bc71e9d3d95958347e6
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2022-12-01 00:19:42 +0100

    feat: update package and project configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,23 @@
 module.exports = {
   root: true,
-  ignorePatterns: ['projects/**/*'],
+  ignorePatterns: ['projects/**/*', '**/*.js'],
   overrides: [
     {
       files: ['*.ts'],
       parserOptions: {
         project: ['tsconfig.json'],
         createDefaultProgram: true,
+        ecmaVersion: 'latest',
       },
+      env: { browser: true },
       extends: [
-        'prettier',
+        'eslint:recommended',
+        'airbnb-base',
+        'airbnb-typescript/base',
+        'plugin:lit-a11y/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:@typescript-eslint/strict',
         'plugin:@angular-eslint/recommended',
         'plugin:@angular-eslint/template/process-inline-templates',
         'plugin:promise/recommended',
@@ -17,6 +25,7 @@ module.exports = {
         'prettier',
       ],
       plugins: [
+        'lit-a11y',
         'simple-import-sort',
         'promise',
         'unicorn',
@@ -24,34 +33,77 @@ module.exports = {
         'import',
       ],
       rules: {
-        'prettier/prettier': [
-          'error',
-          {
-            usePrettierrc: true,
-          },
-        ],
-        'no-confusing-arrow': [
-          'error',
-          {
-            allowParens: true,
-          },
-        ],
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+        'no-confusing-arrow': ['error', { allowParens: true }],
         '@angular-eslint/directive-selector': [
           'error',
-          {
-            type: 'attribute',
-            prefix: 'app',
-            style: 'camelCase',
-          },
+          { type: 'attribute', prefix: 'app', style: 'camelCase' },
         ],
         '@angular-eslint/component-selector': [
           'error',
+          { type: 'element', prefix: 'app', style: 'kebab-case' },
+        ],
+        '@typescript-eslint/no-non-null-assertion': 'error',
+        '@angular-eslint/no-input-rename': 'off',
+        'import/prefer-default-export': 'off',
+        '@typescript-eslint/no-shadow': 'error',
+        '@typescript-eslint/no-use-before-define': [
+          'error',
           {
-            type: 'element',
-            prefix: 'app',
-            style: 'kebab-case',
+            functions: false,
+            classes: true,
+            variables: true,
+            typedefs: true,
           },
         ],
+        'promise/catch-or-return': 'error',
+        'no-underscore-dangle': 'off',
+        '@typescript-eslint/no-extraneous-class': 'off',
+        // #region  //*=========== Import Sort ===========
+        'sort-imports': 'off',
+        'import/first': 'error',
+        'import/newline-after-import': 'error',
+        'import/no-duplicates': 'error',
+        'import/order': 'off',
+        'simple-import-sort/exports': 'error',
+        'simple-import-sort/imports': [
+          'warn',
+          {
+            groups: [
+              // ext library & side effect imports
+              ['^@?\\w', '^\\u0000'],
+              // {s}css files
+              ['^.+\\.s?css$'],
+              // Lib and hooks
+              ['^@/lib', '^@/hooks'],
+              // static data
+              ['^@/data'],
+              // components
+              ['^@/components', '^@/container'],
+              // zustand store
+              ['^@/store'],
+              // Other imports
+              ['^@/'],
+              ['^~/'],
+              // relative paths up until 3 level
+              [
+                '^\\./?$',
+                '^\\.(?!/?$)',
+                '^\\.\\./?$',
+                '^\\.\\.(?!/?$)',
+                '^\\.\\./\\.\\./?$',
+                '^\\.\\./\\.\\.(?!/?$)',
+                '^\\.\\./\\.\\./\\.\\./?$',
+                '^\\.\\./\\.\\./\\.\\.(?!/?$)',
+              ],
+              ['^@/types'],
+              // other that didnt fit in
+              ['^'],
+            ],
+          },
+        ],
+        'import/extensions': 'off',
+        // #endregion  //*======== Import Sort ===========
       },
     },
     {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,42 @@
+const {
+  CloudConfig,
+  URLConfig,
+  CloudinaryImage,
+} = require('@cloudinary/url-gen');
+const babMorocco = getImage('trakz/assets/bab_morocco.webp');
+const nightDusk = getImage('trakz/assets/night_dusk.webp');
+const nightBeach = getImage('trakz/assets/night_beach.webp');
+const mountainBeach = getImage('trakz/assets/mountain_beach.webp');
+const dune = getImage('trakz/assets/dune.webp');
+const street = getImage('trakz/assets/street.webp');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: '480px',
+        '2xs': '540px',
+      },
+      backgroundImage: {
+        'night-bab': `url('${babMorocco.toURL()}')`,
+        'night-dusk': `url('${nightDusk.toURL()}')`,
+        'night-beach': `url('${nightBeach.toURL()}')`,
+        'mountain-beach': `url('${mountainBeach.toURL()}')`,
+        dune: `url('${dune.toURL()}')`,
+        street: `url('${street.toURL()}')`,
+      },
+    },
   },
   plugins: [],
 };
+
+function getImage(publicId, quality = 75) {
+  // Set the Cloud configuration and URL configuration
+  const cloudConfig = new CloudConfig({ cloudName: 'mfaouzi' });
+  const urlConfig = new URLConfig({ secure: true });
+  return new CloudinaryImage(publicId, cloudConfig, urlConfig)
+    .format('webp')
+    .quality(quality);
+}
